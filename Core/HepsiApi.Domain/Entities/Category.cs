58d34afd92400c74a2656df99d203d588565cc51
SelmanKaya <selmankaya3412@gmail.com>
2025-11-16 20:47:03 +0300

    temel enttyler olusturuldu

---
+++ Core/HepsiApi.Domain/Entities/Category.cs
@@ -0,0 +1,28 @@
+﻿using HepsiApi.Domain.common;
+using HepsiApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+
+        public Category() { }
+        public Category(int parentId, string name, int priorty)
+        {
+            this.parentId = parentId;
+            this.name = name;
+            this.priorty = priorty;
+        }
+        public required int parentId { get; set; }
+        public required string name { get; set; }
+        public required int priorty { get; set; }
+
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Core/HepsiApi.Domain/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using HepsiApi.Domain.common;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Domain.Entities
+{
+    public class Product : EntityBase
+    {
+        public required string title { get; set; }
+        public required string description { get; set; }
+
+
+        public required int brandId { get; set; }
+        public required decimal price { get; set; }
+        public required decimal discount { get; set; }
+
+        public Brand Brand { get; set; }
+        public ICollection<Category> Categories { get; set; }
+
+        //public required string imagePath { get; set; }
+
+    }
+}
