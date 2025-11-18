036e69df6de4c43d5c246147d9a825a24c606fdd
SelmanKaya <selmankaya3412@gmail.com>
2025-11-18 18:17:10 +0300

    fake veriler uretildi

---
+++ Infrastructure/HepsiApi.Persistance/Configurations/BrandConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using HepsiApi.Domain.Entities;
+using Bogus;
+
+namespace HepsiApi.Persistance.Configurations
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(x => x.name).HasMaxLength(256);
+            Faker faker = new("tr");
+
+            Brand brand1 = new()
+            {
+                id = 1,
+                name = faker.Commerce.Department(),
+                createdDate = DateTime.Now,
+                isDeleted = false
+            };
+            Brand brand2 = new()
+            {
+                id = 2,
+                name = faker.Commerce.Department(),
+                createdDate = DateTime.Now,
+                isDeleted = false
+            };
+            Brand brand3 = new()
+            {
+                id = 3,
+                name = faker.Commerce.Department(),
+                createdDate = DateTime.Now,
+                isDeleted = true
+            };
+
+            builder.HasData(brand1, brand2, brand3);
+
+        }
+    }
+}
---
+++ Infrastructure/HepsiApi.Persistance/Configurations/ProductConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using HepsiApi.Domain.Entities;
+using Bogus;
+
+namespace HepsiApi.Persistance.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            Faker faker = new("tr");
+            Product product1 = new()
+            {
+                id = 1,
+                title = faker.Commerce.ProductName(),
+                description = faker.Commerce.ProductDescription(),
+                brandId = 1,
+                discount = faker.Random.Decimal(0,10),
+                price = faker.Finance.Amount(10,1000),
+                createdDate = DateTime.Now,
+                isDeleted = false
+            };
+            Product product2 = new()
+            {
+                id = 2,
+                title = faker.Commerce.ProductName(),
+                description = faker.Commerce.ProductDescription(),
+                brandId = 3,
+                discount = faker.Random.Decimal(0, 10),
+                price = faker.Finance.Amount(10, 1000),
+                createdDate = DateTime.Now,
+                isDeleted = false
+            };
+            builder.HasData(product1, product2);
+        }
+    }
+    
+    }
+}
